01aad424513dc35ea5dc752e59a3ad7347053c64
amitjalui <amitjalui5@gmail.com>
2023-04-25 01:44:20 +0530

    nextjs setup & Header implementation

---
+++ src/app/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/app/styles/navbar.module.css"
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className={styles.main_header}>
+      <div className={styles.navbar_brand}>
+        <Link href="/">
+          <Image 
+            alt="logo" 
+            src="/logo.png" 
+            width={150}
+            height={40}
+          />
+        </Link>
+      </div>
+
+      <Nav />
+    </header>
+  )
+};
+
+export default Header;
